51c299b85c3370c8eb93e78c9465d57bb0e40f42
Artem Storozhuk <storojs72@gmail.com>
2024-10-04 16:11:21 +0300

    feat: Program for generating Plonk proof using latest sphinx

--- README.md
+++ README.md
@@ -2,16 +2,56 @@
 
 This repository contains smart contracts required for on-chain verification of [Sphinx](https://github.com/lurk-lab/sphinx) proofs.
 
-To install these contracts in your Foundry project:
+To install Solidity contracts in your Foundry project:
 
 ```
 forge install argumentcomputer/sphinx-contracts --no-commit
 ```
 
+To install Move contracts, add following dependency to your Move.toml file:
+
+```
+plonk-core = { git = "https://github.com/argumentcomputer/sphinx-contracts.git", rev = "main", subdir = "move" }
+```
+
+and also use `plonk_verifier_addr`
+
 # Updating the contracts
 
-To update the contracts, just download Sphinx artifacts using specific version and copy *.sol files into `contracts/src`:
+To update the Solidity contracts, just download Sphinx artifacts using specific version and copy *.sol files into `contracts/src`:
 ```
 wget https://sphinx-plonk-params.s3.amazonaws.com/<VERSION>.tar.gz
 cp ~/.sp1/circuits/plonk_bn254/<VERSION>/*.sol contracts/src/
 ```
+
+The Move contracts need to be updated manually, by looking at actual Solidity diff.
+Usually contracts update is actually a changing of the constants' values.
+
+In order to test the new version of contracts, generate proof using new sphinx version:
+
+```
+RUST_LOG=info cargo run --package sphinx-proof --release
+```
+
+then copy-paste output to the relevant places in Move / Solidity tests.
+
+Finally, to test updated Solidity contracts:
+
+```
+cd contracts
+forge test
+```
+
+and Move contracts:
+
+```
+aptos move compile --named-addresses plonk_verifier_addr=testnet
+aptos move test --named-addresses plonk_verifier_addr=testnet
+```
+
+Additionally, it is necessary to publish (deploy) Move contract in order to re-use it as a dependency in higher-level project (in Aptos testnet):
+
+```
+aptos move publish --named-addresses plonk_verifier_addr=testnet --profile testnet --assume-yes
+```
+
---
+++ sphinx-proof/main.rs
@@ -0,0 +1,92 @@
+use itertools::Itertools;
+use sha2::{Digest, Sha256};
+use sphinx_sdk::utils::setup_logger;
+use sphinx_sdk::{
+    HashableKey, ProverClient, SphinxProofWithPublicValues, SphinxStdin, SphinxVerifyingKey,
+};
+
+fn print_solidity(vk: &SphinxVerifyingKey, proof: &SphinxProofWithPublicValues) {
+    println!("Solidity");
+    println!();
+    println!(
+        "bytes private TestVerifierKey = abi.encodePacked(hex\"{}\");",
+        vk.bytes32().to_string().as_str()[2..].to_string()
+    );
+    println!(
+        "bytes private TestPublicValues = abi.encodePacked(hex\"{}\");",
+        proof.public_values.bytes().to_string().as_str()[2..].to_string()
+    );
+    match &proof.proof {
+        sphinx_sdk::SphinxProof::Plonk(pr) => {
+            println!(
+                "bytes private TestProof = abi.encodePacked(hex\"{}\");",
+                format!(
+                    "{}{}",
+                    hex::encode(&pr.plonk_vkey_hash[..4]),
+                    pr.encoded_proof,
+                )
+            );
+        }
+        _ => unreachable!(),
+    };
+
+    println!();
+}
+
+fn print_move(vk: &SphinxVerifyingKey, proof: &SphinxProofWithPublicValues) {
+    println!("Move");
+    println!();
+    println!(
+        "const SphinxInclusionProofVk: u256 = 0x{};",
+        vk.bytes32().to_string().as_str()[2..].to_string()
+    );
+
+    let raw_public_values =
+        hex::decode(proof.public_values.bytes().to_string().as_str()[2..].to_string()).unwrap();
+
+    println!(
+        "const SphinxRawPublicValues: vector<u8> = x\"{}\";",
+        hex::encode(&raw_public_values).replace("\"", "")
+    );
+
+    let res = Sha256::digest(&raw_public_values);
+    let mut hash = [0u8; 32];
+    hash.copy_from_slice(&res);
+    hash[0] &= 0x1f;
+
+    println!(
+        "const SphinxPublicValuesHash: u256 = 0x{};",
+        hex::encode(&hash).replace("\"", "")
+    );
+
+    match &proof.proof {
+        sphinx_sdk::SphinxProof::Plonk(pr) => {
+            let proof_chunks = pr
+                .encoded_proof
+                .chars()
+                .collect::<Vec<char>>()
+                .chunks(64)
+                .map(|c| c.iter().collect::<String>())
+                .collect::<Vec<String>>();
+
+            for (i, chunk) in proof_chunks.into_iter().enumerate() {
+                println!("const Proof_chunk_{}: u256 = 0x{};", i, chunk);
+            }
+        }
+        _ => unreachable!(),
+    };
+}
+
+fn main() {
+    setup_logger();
+    let prover = ProverClient::new();
+    let mut stdin = SphinxStdin::new();
+    stdin.write(&Vec::<u8>::new());
+
+    let (pk, vk) = prover.setup(include_bytes!("../riscv32im-succinct-zkvm-elf"));
+    let proof = prover.prove(&pk, stdin).plonk().run().unwrap();
+    prover.verify(&proof, &vk).unwrap();
+
+    print_solidity(&vk, &proof);
+    print_move(&vk, &proof);
+}
